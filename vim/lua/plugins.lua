48cbcf5119c425c103e28b2afd104b7f55216274
gnomeman <ns6577@yahoo.com>
2023-10-08 21:47:41 -0400

    feat: updates

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,17 +1,19 @@
-require("paq")({
-	-- Dependencies
-	"savq/paq-nvim",
-	"nvim-lua/plenary.nvim",
-	"neovim/nvim-lspconfig",
-	"nvim-treesitter/nvim-treesitter",
-	"vim-ctrlspace/vim-ctrlspace",
-	-- Git
-	"TimUntersberger/neogit",
-	-- Autocomplete
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	-- Debugger
-	"mfussenegger/nvim-dap",
-	"rcarriga/nvim-dap-ui",
-	"leoluz/nvim-dap-go",
-})
+require("paq")(
+  {
+    -- Dependencies
+    "savq/paq-nvim",
+    "nvim-lua/plenary.nvim",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+    "vim-ctrlspace/vim-ctrlspace",
+    -- Git
+	"tpope/vim-fugitive",
+    -- Autocomplete
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    -- Debugger
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    "leoluz/nvim-dap-go",
+  }
+)
