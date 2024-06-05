a8e84e5aabafbd5725ba2a1a629fd1c18b145a9d
Noman Sarker <35546305+gnomeman@users.noreply.github.com>
2024-06-05 07:23:46 -0400

    Feat/luaide (#3)
    * feat: updates
    
    * fix: minor ide

--- vim/lua/lua/ide.lua
+++ vim/lua/lua/ide.lua
@@ -5,71 +5,73 @@ local vim_split = vim.split
 -- Set up the autocomplete logic necessary for listing out LSP options.
 require("lua.autocomplete")
 
-local PATH_LUA_LSP_MAIN = "/PATH/TO/LUA/LSP/SERVER"
+local PATH_LUA_LSP_MAIN = "/home/luthor/Projects/Github/lua-language-server/main.lua"
 
 local opts = {
-	noremap = true,
-	silent = true,
+  noremap = true,
+  silent = true,
 }
 
 local settings = {
-	Lua = {
-		runtime = {
-			version = "Lua 5.1",
-			--version = "Lua 5.3.3",
-			path = {
-				"?.lua",
-				"?/init.lua",
-				vim_split(package.path, ";"),
-				-- v5.1 or Luajit
-				"/usr/lib/luarocks/rocks-5.1/",
-				-- v5.3
-				--"/usr/local/lib/luarocks/rocks-5.3/?/init.lua",
-				--"/usr/local/lib/lua/5.3/?/init.lua",
-			},
-		},
-		workspace = {
-			library = {
-				-- Project path
-				os.getenv("PWD"),
-				-- v5.1 or Luajib
-				"/usr/lib/luarocks/rocks-5.1/",
-				-- v5.3
-				--"/usr/local/lib/luarocks/rocks-5.3",
-				--"/usr/local/lib/lua/5.3",
-			},
-		},
-		diagnostics = {
-			globals = {
-				"vim",
-			},
-		},
-	},
+  Lua = {
+    runtime = {
+      version = "Lua 5.1",
+      -- version = "Lua 5.3.3",
+      path = {
+        "?.lua",
+        "?/init.lua",
+        vim_split(package.path, ";"),
+        -- v5.1 or Luajit
+        "/usr/lib/luarocks/rocks-5.1/",
+        -- v5.3
+        -- "/usr/local/lib/luarocks/rocks-5.3/?/init.lua",
+        -- "/usr/local/lib/lua/5.3/?/init.lua",
+      },
+    },
+    workspace = {
+      library = {
+        -- Project path
+        os.getenv("PWD"),
+        -- v5.1 or Luajib
+        "/usr/lib/luarocks/rocks-5.1/",
+        -- v5.3
+        -- "/usr/local/lib/luarocks/rocks-5.3",
+        -- "/usr/local/lib/lua/5.3",
+      },
+    },
+    diagnostics = {
+      globals = {
+        "vim",
+      },
+    },
+  },
 }
 
 local function on_attach(client, bufnr)
-	local function buf_set_keymap(...)
-		nvim_buf_set_keymap(bufnr, ...)
-	end
+  local function buf_set_keymap(...)
+    nvim_buf_set_keymap(bufnr, ...)
+  end
 
-	-- Key bindings
-	buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "ga", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "<leader>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  -- Key bindings
+  buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("n", "<leader>R", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "<leader>K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "<leader>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()(0, {scope='line'})<CR>", opts)
+
 end
 
-require("lspconfig").lua_ls.setup({
-	cmd = {
-		"lua-language-server",
-		"-E",
-		fn_expand(PATH_LUA_LSP_MAIN),
-	},
-	settings = settings,
-	on_attach = on_attach,
-	capabilities = require("cmp_nvim_lsp").default_capabilities(),
-})
+require("lspconfig").lua_ls.setup(
+  {
+    cmd = {
+      "lua-language-server",
+      "-E",
+      fn_expand(PATH_LUA_LSP_MAIN),
+    },
+    settings = settings,
+    on_attach = on_attach,
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  }
+)
--- vim/lua/settings.lua
+++ vim/lua/settings.lua
@@ -7,7 +7,7 @@ local set_keymap = vim.api.nvim_set_keymap
 -- Constants
 local LEADER = "<leader>"
 local NOREMAP_TRUE = {
-	noremap = true,
+  noremap = true,
 }
 
 -- Map leader to space
@@ -21,15 +21,15 @@ set_keymap("i", "jk", "<Esc>", NOREMAP_TRUE)
 
 -- Clipboard
 o.clipboard = o.clipboard .. "unnamedplus"
-set_keymap("v", LEADER .. "y", '"+y', NOREMAP_TRUE)
-set_keymap("n", LEADER .. "y", '"+y', NOREMAP_TRUE)
-set_keymap("n", LEADER .. "Y", '"+yg', NOREMAP_TRUE)
-set_keymap("n", LEADER .. "yy", '"+yy', NOREMAP_TRUE)
+set_keymap("v", LEADER .. "y", "\"+y", NOREMAP_TRUE)
+set_keymap("n", LEADER .. "y", "\"+y", NOREMAP_TRUE)
+set_keymap("n", LEADER .. "Y", "\"+yg", NOREMAP_TRUE)
+set_keymap("n", LEADER .. "yy", "\"+yy", NOREMAP_TRUE)
 
-set_keymap("v", LEADER .. "p", '"+p', NOREMAP_TRUE)
-set_keymap("v", LEADER .. "P", '"+P', NOREMAP_TRUE)
-set_keymap("n", LEADER .. "p", '"+p', NOREMAP_TRUE)
-set_keymap("n", LEADER .. "P", '"+P', NOREMAP_TRUE)
+set_keymap("v", LEADER .. "p", "\"+p", NOREMAP_TRUE)
+set_keymap("v", LEADER .. "P", "\"+P", NOREMAP_TRUE)
+set_keymap("n", LEADER .. "p", "\"+p", NOREMAP_TRUE)
+set_keymap("n", LEADER .. "P", "\"+P", NOREMAP_TRUE)
 
 -- Colors
 o.termguicolors = true
@@ -41,6 +41,9 @@ cmd("set cursorline!") -- Have to set CursorLineNr to show line number colors
 
 -- Cursor
 o.virtualedit = "onemore"
+set_keymap("n", "j", "gj", NOREMAP_TRUE)
+set_keymap("n", "k", "gk", NOREMAP_TRUE)
+o.mouse = ""
 
 -- Search configuration
 o.hlsearch = true
@@ -51,11 +54,13 @@ o.incsearch = true
 
 -- Autocomplete
 o.completeopt = "menuone,longest,noselect,noinsert"
+-- Disable annoying autoindent
+o.indentkeys = ""
 
 -- Spacing
-o.tabstop = 4
-o.shiftwidth = 4
-o.softtabstop = 4
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
 
 -- Remove trailing whitespace
 cmd("autocmd FileType golang,javascript,lua,python autocmd BufWritePre <buffer> %s/\\s\\+$//e")
