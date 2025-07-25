244ffc7cc5ffc930329139025806f7054cc2566c
gnomeman <ns6577@yahoo.com>
2025-07-25 10:04:49 -0400

    feat: add border for diagnostic keymaps

--- vim/lua/internal/keymap.lua
+++ vim/lua/internal/keymap.lua
@@ -21,51 +21,57 @@ end
 -- @param cbuf Current buffer.
 -- @return table
 function _M.base_ide_keymaps(cbuf)
+	local float_w_border = {border = "rounded"}
+
 	keymap(
 		"n", "gd", vim.lsp.buf.definition, {
 			desc = "Jumps to the definition of the symbol under the cursor.",
-			buffer = bufnr,
+			buffer = cbuf,
 		}
 	)
 	keymap(
 		"n", "gr", vim.lsp.buf.references, {
 			desc = "Lists all the references to the symbol under the cursor in the quickfix window.",
-			buffer = bufnr,
+			buffer = cbuf,
 		}
 	)
 	keymap(
 		"n", "<leader>R", vim.lsp.buf.rename, {
 			desc = "Renames all references to the symbol under the cursor.",
-			buffer = bufnr,
+			buffer = cbuf,
 		}
 	)
 	keymap(
-		"n", "<leader>K", vim.lsp.buf.hover, {
+		"n", "<leader>K", function(_) vim.lsp.buf.hover(float_w_border) end, {
 			desc = "Displays hover information about the symbol under the cursor in a floating window.",
 		}
 	)
 	keymap(
-		"n", "<leader>k", vim.lsp.buf.signature_help, {
+		"n", "<leader>k", function(_) vim.lsp.buf.signature_help(float_w_border) end, {
 			desc = "Displays signature information about the symbol under the cursor in a floating window.",
-			buffer = bufnr,
+			buffer = cbuf,
 		}
 	)
 	keymap(
 		"n", "<leader>q", vim.diagnostic.setloclist, {
 			desc = "Add buffer diagnostics to the location list.",
-			buffer = bufnr,
+			buffer = cbuf,
 		}
 	)
 	keymap(
-		"n", "<leader>e", function()
+		"n", "<leader>e", function(_)
 			vim.diagnostic.open_float(
-				0, {
-					scope = "line",
-				}
+				0, vim.tbl_extend(
+					"force",
+					float_w_border,
+					{
+						scope = "line",
+					}
+				)
 			)
 		end, {
 			desc = "Display diagnostic message.",
-			buffer = bufnr,
+			buffer = cbuf,
 		}
 	)
 end
