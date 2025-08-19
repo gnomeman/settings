9479d59b77eac00b2c0a51cad8772c94be4c79e5
gnomeman <ns6577@yahoo.com>
2025-08-19 09:49:11 -0400

    feat: auto install paq and plugins

--- vim/init.lua
+++ vim/init.lua
@@ -1,54 +1,62 @@
 -- Use _loadout for loading configs initially, then remove it.
-rawset(_G, "_loadout", {
-	plugins = {
-    -- Dependencies
-    "savq/paq-nvim",
-    "nvim-lua/plenary.nvim",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-neotest/nvim-nio",
-    -- Git
-    "tpope/vim-fugitive",
-    -- Autocomplete
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    -- Debugger
-    "mfussenegger/nvim-dap",
-    "rcarriga/nvim-dap-ui",
-    "leoluz/nvim-dap-go",
-		-- Rust
-		"mrcjkb/rustaceanvim",
-    -- Telescope
-    "nvim-telescope/telescope.nvim",
-    -- Snippets
-    "hrsh7th/vim-vsnip",
-    "hrsh7th/vim-vsnip-integ",
-    "hrsh7th/cmp-vsnip",
-    "rafamadriz/friendly-snippets",
-    -- HTTP request
-    "mistweaverco/kulala.nvim",
-    -- Database
-    "tpope/vim-dadbod",
-    "kristijanhusak/vim-dadbod-ui",
-    "kristijanhusak/vim-dadbod-completion",
-    -- Quality of life
-    "miversen33/sunglasses.nvim",
-    {
-      "ThePrimeagen/harpoon",
-      branch = "harpoon2",
-    }
-	}
-})
+rawset(
+  _G, "_loadout", {
+    plugins = {
+      -- Dependencies
+      "savq/paq-nvim",
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/nvim-nio",
+      -- Git
+      "tpope/vim-fugitive",
+      -- Autocomplete
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      -- Debugger
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+      -- Rust
+      "mrcjkb/rustaceanvim",
+      -- Telescope
+      "nvim-telescope/telescope.nvim",
+      -- Snippets
+      "hrsh7th/vim-vsnip",
+      "hrsh7th/vim-vsnip-integ",
+      "hrsh7th/cmp-vsnip",
+      "rafamadriz/friendly-snippets",
+      -- HTTP request
+      "mistweaverco/kulala.nvim",
+      -- Database
+      "tpope/vim-dadbod",
+      "kristijanhusak/vim-dadbod-ui",
+      "kristijanhusak/vim-dadbod-completion",
+      -- Quality of life
+      "miversen33/sunglasses.nvim",
+      {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+      },
+    },
+    state = {},
+  }
+)
 
 -- Load local machine logic.
 local path_local_machine = os.getenv("NVIM_PATH_LOCAL_MACHINE")
 if path_local_machine then
-	require(path_local_machine)
+  require(path_local_machine)
 end
 
 require("settings")
 require("plugins")
+if _loadout.state.plugins_just_installed then
+  -- Restart is required after plugins are initially installed.
+  return
+end
+
 require("ide")
 
 -- Remove _loadout.
--- rawset(_G, "_loadout", nil)
+rawset(_G, "_loadout", nil)
--- vim/lua/plugins/init.lua
+++ vim/lua/plugins/init.lua
@@ -1,3 +1,37 @@
+-- Check if Paq is installed. If not, then install.
+local paq_path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+local paq_is_installed = vim.fn.empty(vim.fn.glob(paq_path)) == 0
+if not paq_is_installed then
+  vim.fn.system(
+    {
+      "git",
+      "clone",
+      "--depth=1",
+      "https://github.com/savq/paq-nvim.git",
+      paq_path,
+    }
+  )
+
+  -- Exit after PaqInstall.
+  vim.cmd("autocmd User PaqDoneInstall quit")
+
+  -- Load plugins and install.
+  vim.cmd.packadd("paq-nvim")
+  local paq = require("paq")
+  paq(_loadout.plugins)
+  vim.ui.input(
+    {
+      prompt = "Installing plugins. Afterwards, the system will exit automatically. Press ENTER to proceed.",
+    }, function(_)
+    end
+  )
+  paq.install()
+
+  -- Set the state of plugins.
+  _loadout.state.plugins_just_installed = true
+  return
+end
+
 -- Load global list of plugins.
 require("paq")(_loadout.plugins)
 
